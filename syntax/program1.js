9bdaec4b828c13faa526245baf13c3a6a938d3e5
seungchan bae <bea0266@naver.com>
2021-05-16 02:51:01 +0900

    nodejs first time

---
+++ syntax/program1.js
@@ -0,0 +1,7 @@
+var num1=10;
+var num2=20;
+if(num1==num2)
+  console.log("정답");
+else {
+  console.log("다르다");
+}
---
+++ syntax/string.js
@@ -0,0 +1,8 @@
+var name='bae';
+
+var letter = `${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporcia deserunt mollit anim id est laborum.
+
+ ${name}`;
+console.log(letter);
